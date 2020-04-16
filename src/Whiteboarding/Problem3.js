b9d906f165062fd092660bde9450afccaa63a381
Krista Rutz <rutzkri000@gmail.com>
2020-04-16 10:15:14 -0700

    Vera whiteboards fibonacci
    Co-authored-by: Dusty Mccord <dustymccord@gmail.com>
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>
    Co-authored-by: acolistro <arcolistro@gmail.com>
    Co-authored-by: MallSoft95 <arcolistro@gmail.com>
    Co-authored-by: Dusty <dustymccord@gmail.com>
    Co-authored-by: VW <vera.weikel@gmail.com>

--- src/Whiteboarding/Problem3.js
+++ src/Whiteboarding/Problem3.js
@@ -4,11 +4,3 @@ Largest prime factor
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
-*/
-const fib = (n, a = 0, b = 1) => {
-  if (0 < n) {
-    fib(--n, b, a + b);
-  } else {
-    a;
-  }
-};
